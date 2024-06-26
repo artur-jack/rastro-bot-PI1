4ecc9b4353b0bb6378e4f1d6ce1c094a16db63be
Artur Jackson <100738244+artur-jack@users.noreply.github.com>
2024-06-26 17:29:54 -0300

    Delete rastrobotDjango directory

--- rastrobotDjango/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import ESP32Data
-
-class ESP32DataSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ESP32Data
-        fields = ['sensor_esquerdo', 'sensor_direito', 'velocidade', 'distancia', 'timestamp']
-        read_only_fields = ['timestamp']
